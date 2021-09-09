a8cd0f8d2a63ea48598cf1b8415f2e2fc4cafff0
Zhong Yunlong <zhongyunlong@bytedance.com>
2021-09-10 03:26:09 +0800

    ✨支持蚁群演示算法随机生成节点，并限制不能与已有节点靠得太近

--- aco.lua
+++ aco.lua
@@ -1,10 +1,12 @@
 -- ant colony optimization
 local Scene = require 'scene'
 local Sprite = require 'sprite'
+local _ = require 'underscore'
 
 local ACO = Scene:subclass 'ACO'
 local Node = Sprite:subclass 'Node'
 local ByteOfA = string.byte 'A'
+local FarEnoughRadius = 60
 
 function Node:draw()
     love.graphics.setColor(1, 1, 1)
@@ -15,16 +17,61 @@ end
 
 function ACO:initialize(config)
     Scene.initialize(self, config)
-    self.nodes = {}
+    self:generate(10)
 end
 
-function ACO:mousepressed(x, y)
+function ACO:addNode(x, y)
     local newNode = Node {
         x = x,
         y = y,
         label = string.char(ByteOfA + #self.nodes)
     }
     table.insert(self.nodes, self:addSprite(newNode))
+end
+
+function ACO:mousepressed(x, y)
+    if self:farEnough(x, y) then
+        self:addNode(x, y)
+    end
+end
+
+function ACO:farEnough(x, y)
+    local squaredDistance = FarEnoughRadius * FarEnoughRadius
+    return _.all(self.nodes, function(node)
+        local dx = x - node.x
+        local dy = y - node.y
+        local squaredSum = dx * dx + dy * dy
+        return squaredSum >= squaredDistance
+    end)
+end
+
+function ACO:generate(n)
+    self.sprites = {}
+    self.nodes = {}
+    local margin = 50
+    local width = love.graphics.getWidth()
+    local height = love.graphics.getHeight()
+
+    for i = 1, n do
+        local x, y
+        repeat
+            x = love.math.random(margin, width - margin)
+            y = love.math.random(margin, height - margin)
+        until self:farEnough(x, y)
+
+        self:addNode(x, y)
+    end
+end
+
+function ACO:keyreleased(key)
+    if key == 'return' then
+        -- start algorithm
+    elseif key == 'c' then
+        self.sprites = {}
+        self.nodes = {}
+    elseif key == 'g' then
+        self:generate(10)
+    end
 end
 
 function ACO:switched()
